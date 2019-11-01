c446e00d311c488f7b85cbfb4c556857890c12a0
Abdel Saïd <abdelsaidt@gmail.com>
2019-11-01 16:45:25 +0100

    update test_autom

--- tst/test_autom.c
+++ tst/test_autom.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "../src/autom.h"
 #include "../src/graph.h"
+#include <string.h>
 
 
 #define NB_FINALS_STATES 2
@@ -12,8 +13,9 @@ int main()
     int nb_states = 5;
     int finals_st[NB_FINALS_STATES] = {2,  4};
     int nb_finals = NB_FINALS_STATES;
-    char alphabet[CARD_ALPHB] = "abcd";
+    char alphabet[CARD_ALPHB] = "abcd", word[20];
     int initial = 0;
+
 
     Automate *autom = create_automate(nb_states, alphabet, initial, finals_st, nb_finals);
 
@@ -22,50 +24,30 @@ int main()
     add_transition(autom,1,'b',4);
     add_transition(autom,0,'c',3);
     add_transition(autom,3,'b',1);
+    add_transition(autom,3,'a',4);
     add_transition(autom,1,'d',1);
+    add_transition(autom,1,'b',2);
+    add_transition(autom,2,'c',3);
+
+
 
     print_autom(autom);
     
-    if(accept(autom, "ab"))
-    {
-        printf("Trv \n");
-    }
-    else
-    {
-        printf("Pas trv \n");
-    }
-
-    if(accept(autom, "cb"))
-    {
-        printf("Trv \n");
-    }
-    else
-    {
-        printf("Pas trv \n");
-    }
-
+    strcpy(word, "ab\0");
+    printf("%s find in automate ? %d \n", word, accept(autom, word));
+   
+    strcpy(word, "cb");
+    printf("%s find in automate ? %d \n", word, accept(autom, word));
+   
+    strcpy(word, "cc");
+    printf("%s find in automate ? %d \n", word, accept(autom, word));   
     
-    if(accept(autom, "cc"))
-    {
-        printf("Trv \n");
-    }
-    else
-    {
-        printf("Pas trv \n");
-    }
 
-    if(accept(autom, "adddb"))
-    {
-        printf("Trv \n");
-    }
-    else
-    {
-        printf("Pas trv \n");
-    }
+    strcpy(word, "abcbddb");
+    printf("%s find in automate ? %d \n", word, accept(autom, word));
 
-
-
-
+    strcpy(word, "abcbdd");
+    printf("%s find in automate ? %d \n", word, accept(autom, word));
 
     
     return 0;
